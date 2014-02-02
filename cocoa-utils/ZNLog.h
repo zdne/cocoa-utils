9b611f0abc59ebb7a2d9cca21367b1f89cc53aae
Z <z@apiary.io>
2014-02-03 00:18:39 +0100

    Do no log fuction & line number

--- cocoa-utils/ZNLog.h
+++ cocoa-utils/ZNLog.h
@@ -12,13 +12,13 @@
 #if DEBUG == 1
 #define ZNLogEntry      NSLog(@"ZN-IN: %s:%d", __PRETTY_FUNCTION__,__LINE__)
 #define ZNLogExit       NSLog(@"ZN-OUT: %s:%d", __PRETTY_FUNCTION__,__LINE__)
-#define ZNLog(A, ...)   NSLog(@"ZN: %s:%d:%@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__])
-#define ZNRLog(A, ...)  NSLog(@"ZN-R: %s:%d: %@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__])
+#define ZNLog(A, ...)   NSLog(@"ZN: %@", [NSString stringWithFormat:A, ## __VA_ARGS__])
+#define ZNRLog(A, ...)  NSLog(@"ZN-R: %@",[NSString stringWithFormat:A, ## __VA_ARGS__])
 #else
 #define ZNLogEntry
 #define ZNLogExit
 #define ZNLog(A, ...)
-#define ZNRLog(A, ...)  NSLog(@"ZN-R: %s:%d: %@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__])
+#define ZNRLog(A, ...)  NSLog(@"ZN-R: %s:%d: %@",[NSString stringWithFormat:A, ## __VA_ARGS__])
 #endif
 
 #endif
