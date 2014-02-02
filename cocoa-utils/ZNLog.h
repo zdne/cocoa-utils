9c956245c6a05d696031126285f62947f1375bd4
Z <z@apiary.io>
2014-02-02 15:18:23 +0100

    Remove superfuous semicolons in ZNLog macros.

--- cocoa-utils/ZNLog.h
+++ cocoa-utils/ZNLog.h
@@ -10,15 +10,15 @@
 #define cocoa_utils_ZNLog_h
 
 #if DEBUG == 1
-#define ZNLogEntry      NSLog(@"ZN-IN: %s:%d", __PRETTY_FUNCTION__,__LINE__);
-#define ZNLogExit       NSLog(@"ZN-OUT: %s:%d", __PRETTY_FUNCTION__,__LINE__);
-#define ZNLog(A, ...)   NSLog(@"ZN: %s:%d:%@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__]);
-#define ZNRLog(A, ...)  NSLog(@"ZN-R: %s:%d: %@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__]);
+#define ZNLogEntry      NSLog(@"ZN-IN: %s:%d", __PRETTY_FUNCTION__,__LINE__)
+#define ZNLogExit       NSLog(@"ZN-OUT: %s:%d", __PRETTY_FUNCTION__,__LINE__)
+#define ZNLog(A, ...)   NSLog(@"ZN: %s:%d:%@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__])
+#define ZNRLog(A, ...)  NSLog(@"ZN-R: %s:%d: %@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__])
 #else
 #define ZNLogEntry
 #define ZNLogExit
 #define ZNLog(A, ...)
-#define ZNRLog(A, ...)  NSLog(@"ZN-R: %s:%d: %@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__]);
+#define ZNRLog(A, ...)  NSLog(@"ZN-R: %s:%d: %@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__])
 #endif
 
 #endif
